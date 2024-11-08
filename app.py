c03535f6ec17f9e3dc4f353387cd455411fa7403
Oleg Kalachev <okalachev@gmail.com>
2024-11-08 15:14:04 +0300

    Update app

--- app.py
+++ app.py
@@ -12,18 +12,18 @@ from gpt_interact import prompt as ask_gpt
 rospy.init_node('morsai')
 rospy.loginfo('Start MORSAI')
 
-status_pub = rospy.Publisher('/head/status', Bool, queue_size=10)
-cmd_vel_pub = rospy.Publisher('/head/cmd_vel', Twist, queue_size=10)
+status_pub = rospy.Publisher('/head/status', Bool, queue_size=1)
+cmd_vel_pub = rospy.Publisher('/head/cmd_vel', Twist, queue_size=1)
 
 cmd_vel = Twist()
 
 SYSTEM_PROMPT = '''Ты управляешь роботом-собакой. Тебе нужно написать программу на Python для нее.
-Можешь использовать любые функции Python, включая time.sleep и т. д.
+Можешь использовать любые функции Python, включая time.sleep и т. д. Только не забывай импортировать нужные модули.
 Выдай в ответ только программу на Python, без форматирования и без ```.
 Для управления используй следующие функции:
-set_velocity(x, y, z) - установить скорость движения робота.
+set_velocity(x, y, z, yaw) - установить скорость движения робота. x - это скорость вперед, y - это скорость налево, z - это скорость вверх (не задействовано), yaw — это угловая скорость в рад/с (против часовой).
 Считай, что функции для управления роботом уже объявлены.
-Максимальная скорость движения робота - 1 м/с.
+Максимальная скорость движения робота по x - 0.5 м/с, а по y - 0.2 м/с, по yaw - 0.85 рад/с.
 Код будет выполнять при помощи функции exec.
 Напиши программу в соответствие с запросом пользователя:
 '''
@@ -40,10 +40,13 @@ def stop():
 
 rospy.on_shutdown(stop)
 
-def set_velocity(x, y, z):
+def set_velocity(x, y, z, yaw):
+    if yaw != 0 and abs(x) < 0.1:
+        x = 0.1
     cmd_vel.linear.x = x
     cmd_vel.linear.y = y
     cmd_vel.linear.z = z
+    cmd_vel.angular.z = yaw
     # cmd_vel_pub.publish(cmd_vel)
     # status_pub.publish(True)
 
