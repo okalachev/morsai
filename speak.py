4cc48e06cf89a793cbbd216ce641ac12fc9d36b0
Oleg Kalachev <okalachev@gmail.com>
2024-11-08 18:48:45 +0300

    Commit speaker

---
+++ speak.py
@@ -0,0 +1,15 @@
+import subprocess
+
+
+def speak(text: str):
+    rate = 155
+    pitch = 30
+    voice = 'ru'
+    # subprocess.call(['espeak', '-v', voice, text])
+    subprocess.call(['espeak', '-s', str(rate), '-p', str(pitch), '-v', voice, text])
+
+
+if __name__ == '__main__':
+    # speak('Всем привет!')
+    speak('Гаф гаф!')
+
