4ba95b3fb46b2f750d5de59bcaf9719dfd6623c0
nikilokser <32608425+nikilokser@users.noreply.github.com>
2024-11-09 20:50:50 +0700

    Update speak with openai tts

--- speak.py
+++ speak.py
@@ -1,4 +1,36 @@
 import subprocess
+from pathlib import Path
+from openai import OpenAI
+from pydub import AudioSegment
+import rospy
+from speakers_controller.srv import playSound, playSoundRequest
+# from secret import OPENAI_API_KEY
+
+rospy.init_node("example_speakers_controller_playSound_node")
+
+client = OpenAI(api_key="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", base_url="https://openai.batalov.me/v1")
+
+def speak_ai(text: str):
+    speech_file_path = Path(__file__).parent / "speech.mp3"
+
+    response = client.audio.speech.create(
+    model="tts-1",
+    voice="shimmer",
+    input=text, 
+    response_format="mp3",
+    speed="0.8"
+    )
+
+    response.stream_to_file(speech_file_path)
+    audio = AudioSegment.from_file("/home/pi/morsai/speech.mp3", format="mp3")
+    louder_audio = audio + 20
+    louder_audio.export("/home/pi/morsai/speech_louder.mp3", format="mp3")
+
+    service_playSound = rospy.ServiceProxy('playSound', playSound)
+    request = playSoundRequest()
+    request.FileName = "/home/pi/morsai/speech_louder.mp3"
+    request.IsBreakable = 0
+    service_playSound(request)
 
 
 def speak(text: str):
@@ -10,6 +42,6 @@ def speak(text: str):
 
 
 if __name__ == '__main__':
-    # speak('Всем привет!')
-    speak('Гаф гаф!')
-
+    # speak_ai('Раз, два, три, четыре, пять, я хочу выйти погулять')
+    # speak('Гаф гаф!')
+    pass
